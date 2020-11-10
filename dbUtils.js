ba0bcce4fcab03d982cb957c3e266aa071290365
jdakata <akata.jda@gmail.com>
2020-11-10 19:30:26 +0100

    enable access connection to db from module dbUtils

--- dbUtils.js
+++ dbUtils.js
@@ -1,27 +1,39 @@
-const MongoClient = require( 'mongodb' ).MongoClient;
-const url = 'mongodb://mongo:27017';
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
 const dbName = 'scrumProject';
 let _db;
+let connection;
 
 function connectToServer(callback) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
-        if (err)
-            throw err;
+    connection = MongoClient.connect(url, { useNewUrlParser: true });
 
+    connection.then(function (client) {
         console.log('Connected successfully to server');
         _db = client.db(dbName);
-
-        // Create collection projects
-        _db.createCollection('projects', function (err, res) {
-            if (err) {
-                console.log('Collection projects already exist!');
-            } else {
-                console.log('Collection projects created!');
-            }
-        });
-
-        return callback(null, _db);
+    })
+    .catch(function(err){
+        throw err;
     });
+
+    connection.then(function (client) {
+        // Create collection projects
+        client.db(dbName).createCollection('projects',
+            function (err, res) {
+                if (err) {
+                    console.log('Collection projects already exist!');
+                } else {
+                    console.log('Collection projects created!');
+                }
+            });
+    })
+    .catch(function(err){
+        throw err;
+    });
+
+}
+
+function getConnection() {
+    return connection
 }
 
 function getDb() {
@@ -32,8 +44,8 @@ function getDbName() {
     return dbName;
 }
 
-function getURL(){
+function getURL() {
     return url;
 }
 
-module.exports = { connectToServer, getDb, getDbName, getURL };
+module.exports = { connectToServer, getDb, getDbName, getURL, getConnection };
