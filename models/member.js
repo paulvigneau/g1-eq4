10c8f6b9d2c26e5358110b3c3ff6d28535c87cd8
Silverazz <alexandre.legendre@etu.u-bordeaux.fr>
2020-11-11 02:10:51 +0100

    addMember is working - JD, you need to update project.ejs :)

--- models/member.js
+++ models/member.js
@@ -6,21 +6,14 @@ function addMember(projectId, name, email, role) {
     let dbo = dbUtils.getDb();
 
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    projectModel.getProjectByID(projectId)
-        .then(project => {
-            let memberList = project.members;
+    let memberToAdd = {
+        name: name,
+        email: email,
+        role: role,
+        color: randomColor
+    };
 
-            let memberToAdd = {
-                name: name,
-                email: email,
-                role: role,
-                color: randomColor
-            };
-
-            memberList.push(memberToAdd, function (err, result) {
-                if (err) throw err;
-            });
-        });
+    dbo.collection('projects').update({'_id': new objectId(projectId)}, {$push: { members: memberToAdd }});
 
 }
 
