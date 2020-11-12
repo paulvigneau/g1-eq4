c0d075f0165e7a0950c3d943d5dfdcbf9fdeb490
Silverazz <alexandre.legendre@etu.u-bordeaux.fr>
2020-11-12 22:44:51 +0100

    selenium createproject & displayproject

--- test/projects.test.js
+++ test/projects.test.js
@@ -4,13 +4,19 @@ const app = require('../app');
 const mongoose = require('mongoose');
 const assert = require('assert');
 const chai = require('chai');
+const { describe, it } = require('mocha');
 const chaiHttp = require('chai-http');
 const dirtyChai = require('dirty-chai');
 let projectService = require('../services/project');
+const {Builder, By, until, Key} = require('selenium-webdriver');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 chai.use(dirtyChai);
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .build();
 
 describe('Projects tests', () => {
 
@@ -38,6 +44,63 @@ describe('Projects tests', () => {
                         && new Date(p.end).valueOf() === new Date('2020-11-20').valueOf()
                 ));
             });
+    });
+});
+
+async function saveProject(name, description, start, end) {
+    await driver.get('http://localhost:3000/new-project');
+
+    await driver.findElement(By.id('name'))
+        .then(async element => {
+            await element.sendKeys(name);
+        });
+
+    await driver.findElement(By.id('description'))
+        .then(async element => {
+            await element.sendKeys(description);
+        });
+
+    await driver.findElement(By.id('start'))
+        .then(async element => {
+            await element.sendKeys(start);
+        });
+
+    await driver.findElement(By.id('end'))
+        .then(async element => {
+            await element.sendKeys(end);
+        });
+
+    await driver.findElement(By.css('body > form > button'))
+        .then(async element => {
+            await element.click();
+        });
+}
+
+describe('New project page', () => {
+    it('We should be redirected to the creation page of projects', async () => {
+        await driver.get('http://localhost:3000');
+
+        await driver.findElements(By.className('btn-success'))
+            .then(async elements => {
+                await elements[0].click();
+
+                driver.getCurrentUrl().then( url => {
+                    expect(url.includes('/new-project')).true;
+                });
+            });
+    });
+});
+
+describe('createproject & displayProjects', () => {
+    it('This should add a project and display it in homepage', async () => {
+        await saveProject('Projet 1', 'Ceci est un magnifique projet', '12-11-2020', '20-11-2020');
+        await driver.get('http://localhost:3000/');
+
+        await driver.findElements(By.className('card'))
+            .then(async projects => {
+                expect(projects.length).to.be.equal(2);
+            });
+
     });
 });
 
