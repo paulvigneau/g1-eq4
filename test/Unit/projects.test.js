9de2083b82228470c5c6903d3e9242871f51fbbb
paulvigneau <paulvigneau98@gmail.com>
2020-11-13 12:15:25 +0100

    Add GET '/' test

--- test/Unit/projects.test.js
+++ test/Unit/projects.test.js
@@ -8,17 +8,12 @@ const { describe, it } = require('mocha');
 const chaiHttp = require('chai-http');
 const dirtyChai = require('dirty-chai');
 let projectService = require('../../services/project');
-const {Builder, By, until, Key} = require('selenium-webdriver');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 chai.use(dirtyChai);
 
-let driver = new Builder()
-    .forBrowser('chrome')
-    .build();
-
-describe('Projects tests', () => {
+describe('Projects unit tests', () => {
 
     it('should get all the project stored', async () => {
         const projects = await projectService.getAllProjects();
@@ -32,6 +27,14 @@ describe('Projects tests', () => {
         });
     });
 
+    it('should get return status 200', async () => {
+        let res = await chai
+            .request(app)
+            .get('/')
+            .send();
+
+        expect(res.status).to.equal(200);
+    });
 
     it('should add a new project in database', async () => {
         let res = await chai
