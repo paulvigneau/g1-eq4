a1bfdebb12643fc974a448d83f84b41c5a02bc78
Silverazz <alexandre.legendre@etu.u-bordeaux.fr>
2020-11-13 12:13:25 +0100

    push E2E to fix

--- test/E2E/projects.test.js
+++ test/E2E/projects.test.js
@@ -1,5 +1,3 @@
-process.env.NODE_ENV = 'test';
-
 const mongoose = require('mongoose');
 const assert = require('assert');
 const chai = require('chai');
@@ -17,49 +15,10 @@ let driver = new Builder()
     .forBrowser('chrome')
     .build();
 
-describe('Projects tests', () => {
-
-    it('should get all the project stored', async () => {
-        const projects = await projectService.getAllProjects();
-
-        mongoose.model('project').find({}).exec((err, expectedProjects) => {
-            if (err)
-                assert.fail();
-            else {
-                assert(expectedProjects.length === projects.length);
-            }
-        });
-    });
-
-
-    it('should add a new project in database', async () => {
-        let res = await chai
-            .request(app)
-            .post('/project')
-            .set('content-type', 'application/x-www-form-urlencoded')
-            .send({
-                name: 'Projet Test',
-                description: 'Description de projet de test',
-                start: '2020-11-10',
-                end: '2020-11-20'
-            });
-
-        expect(res.status).to.equal(200);
-
-        const projects = await projectService.getAllProjects();
-        assert(projects.find(
-            (p) => p.name === 'Projet Test'
-                && p.description === 'Description de projet de test'
-                && new Date(p.start).valueOf() === new Date('2020-11-10').valueOf()
-                && new Date(p.end).valueOf() === new Date('2020-11-20').valueOf()
-        ));
-    });
-});
-
 async function saveProject(name, description, start, end) {
     await driver.get('http://localhost:3000');
 
-    await driver.findElements(By.className('btn-success'))
+    await driver.findElement(By.className('btn-success'))
         .then(async element => {
             await element.click();
         });
@@ -102,35 +61,28 @@ describe('New project page', () => {
                     expect(url.includes('/new-project')).true;
                 });
             });
-    });
+    }).timeout(6000);
 });
 
 describe('createproject & displayProjects', () => {
     it('This should add a project and display it in homepage', async () => {
         await saveProject('Projet 1', 'Ceci est un magnifique projet', '12-11-2020', '20-11-2020');
-        //await driver.get('http://localhost:3000/');
-        /*
+
         await driver.findElements(By.className('card'))
             .then(async projects => {
-                expect(projects.length).to.be.equal(2);
-            });
-*/
-        /*await driver.findElement(By.className('card-title')).getText()
-            .then(async text => {
-                expect(text).to.be.equal('Projet: Projet Test');
+                expect(projects.length).to.be.equal(1);
             });
 
-        driver.wait(selenium.until.elementLocated(selenium.By.className('card-title'), timeOut)).then(function () {
+        await driver.findElement(By.className('card-title')).getText()
+            .then(async text => {
+                expect(text).to.be.equal('Projet: Projet 1');
+            });
+
+        /*driver.wait(selenium.until.elementLocated(selenium.By.className('card-title'), timeOut)).then(function () {
             return driver.findElement(selenium.By.name('project_cdp'));
         });*/
 
-    });
-});
-
-after(function(done) {
-    mongoose.model('project').deleteMany({}, () => {
-        mongoose.connection.close(done);
-    });
+    }).timeout(6000);
 });
 
 module.exports = { saveProject };
