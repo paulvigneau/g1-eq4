15372a6d2d491ced7b919fe3a512ac53b5499a23
paulvigneau <paulvigneau98@gmail.com>
2020-11-17 22:18:09 +0100

    Add US and add sprint are now displayed in popups

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,7 +1,13 @@
-function togglePopup() {
-    const e = document.querySelector('.pop-up-wrapper');
-    if(e.style.display === 'block')
-        e.style.display = 'none';
-    else
-        e.style.display = 'block';
+function togglePopup(id = null) {
+    const wrapper = document.querySelector('.pop-up-wrapper');
+    const popup = document.querySelector(id ? id : '.pop-up');
+    console.log(popup);
+    if(wrapper.style.display === 'block') {
+        wrapper.style.display = 'none';
+        popup.style.display = 'none';
+    }
+    else {
+        wrapper.style.display = 'block';
+        popup.style.display = 'block';
+    }
 }
