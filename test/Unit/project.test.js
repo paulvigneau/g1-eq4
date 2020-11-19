3e0e60ac4b15be9012176a952188de594f46d4a7
paulvigneau <paulvigneau98@gmail.com>
2020-11-19 11:44:43 +0100

    Update project.test.js structure and add deleteMember test

--- test/Unit/project.test.js
+++ test/Unit/project.test.js
@@ -4,100 +4,139 @@ const app = require('../../app');
 const mongoose = require('mongoose');
 const assert = require('assert');
 const projectService = require('../../services/project');
+const memberService = require('../../services/member');
 const chai = require('chai');
 const { describe, it } = require('mocha');
 const chaiHttp = require('chai-http');
 const dirtyChai = require('dirty-chai');
-const memberService = require('../../services/member');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 chai.use(dirtyChai);
 
-let project;
-
 describe('Project unit tests', () => {
+    let project;
 
-    before(async function() {
-        project = await projectService.addProject({
-            name: 'Super Projet',
-            description: 'Une description intéressante',
-            start: '2020-10-10',
-            end: '2020-10-20'
+    describe('Project informations', () => {
+        before(async function() {
+            project = await projectService.addProject({
+                name: 'Super Projet',
+                description: 'Une description intéressante',
+                start: '2020-10-10',
+                end: '2020-10-20'
+            });
+        });
+
+        after(function(done) {
+            mongoose.model('project').deleteMany({}, done);
+        });
+
+        it('should get return status 404 and redirect to /404', async () => {
+            let res = await chai
+                .request(app)
+                .get('/projects/notExistingId')
+                .send();
+
+            expect(res).to.have.status(404);
+            expect(res.redirects[0]).contain('/404');
+
+        });
+
+        it('should get an existing project from its Id', async () => {
+            let res = await chai
+                .request(app)
+                .get('/projects/' + project._id)
+                .send();
+
+            expect(res).to.have.status(200);
+
+            const p = await projectService.getProjectByID(project._id);
+            assert(p.name === 'Super Projet'
+                && p.description === 'Une description intéressante'
+                && new Date(p.start).valueOf() === new Date('2020-10-10').valueOf()
+                && new Date(p.end).valueOf() === new Date('2020-10-20').valueOf()
+            );
+
         });
     });
 
-    it('should get return status 404 and redirect to /404', async () => {
-        let res = await chai
-            .request(app)
-            .get('/projects/notExistingId')
-            .send();
+    describe('New member', () => {
+        let project;
 
-        expect(res).to.have.status(404);
-        expect(res.redirects[0]).contain('/404');
-
-    });
-
-    it('should add a member to a project', async () => {
-        let res = await chai
-            .request(app)
-            .post('/projects/' + project._id + '/member')
-            .set('content-type', 'application/x-www-form-urlencoded')
-            .send({
-                name: 'Bob',
-                role: 'Développeur',
-                email: 'bob@mail.com',
+        before(async function() {
+            project = await projectService.addProject({
+                name: 'Super Projet',
+                description: 'Une description intéressante',
+                start: '2020-10-10',
+                end: '2020-10-20'
             });
+        });
+
+        after(function(done) {
+            mongoose.model('project').deleteMany({}, done);
+        });
+
+        it('should add a member to a project', async () => {
+            let res = await chai
+                .request(app)
+                .post('/projects/' + project._id + '/member')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send({
+                    name: 'Bob',
+                    role: 'Développeur',
+                    email: 'bob@mail.com',
+                });
+
 
         expect(res.status).to.equal(200);
 
-        let p = await projectService.getProjectByID(project._id);
-        assert(p.members.find(
-            (m) => m.name === 'Bob'
+            let p = await projectService.getProjectByID(project._id);
+            assert(p.members.find(
+                (m) => m.name === 'Bob'
                     && m.role === 'Développeur'
                     && m.email === 'bob@mail.com'
-        ));
+            ));
+        });
     });
 
-    // it('should delete a member from a project', async () => {
-    //     let p = await projectService.getProjectByID(project._id);
-    //     await p.members.find(async ( member) => {
-    //
-    //             let res = chai
-    //                 .request(app)
-    //                 .del('/projects/' + p._id + '/members/' + member._id)
-    //                 .set('content-type', 'application/x-www-form-urlencoded');
-    //
-    //             console.log(res);
-    //             expect(res.status).to.equal(200);
-    //
-    //         }
-    //     );
-    //     await console.log(p.members.length);
-    //     await console.log(p.members);
-    //     assert(p.members.length == 1);
-    //
-    // });
+    describe('Delete member', () => {
+        let project;
+        let member;
 
-    it('should get an existing project from its Id', async () => {
-        let res = await chai
-            .request(app)
-            .get('/projects/' + project._id)
-            .send(); 
-            
-        expect(res).to.have.status(200);
+        before(async function() {
+            project = await projectService.addProject({
+                name: 'Super Projet',
+                description: 'Une description intéressante',
+                start: '2020-10-10',
+                end: '2020-10-20'
+            });
+        });
 
-        const p = await projectService.getProjectByID(project._id);
-        assert(p.name === 'Super Projet'
-            && p.description === 'Une description intéressante'
-            && new Date(p.start).valueOf() === new Date('2020-10-10').valueOf()
-            && new Date(p.end).valueOf() === new Date('2020-10-20').valueOf()
-        );
+        after(function(done) {
+            mongoose.model('project').deleteMany({}, done);
+        });
 
+        beforeEach(async function() {
+             member = await memberService.addMember(
+                project._id,
+                'bob',
+                'bob@mail.com',
+                'Développeur'
+            );
+        });
+
+        it('should remove a member', async () => {
+            await memberService.deleteMember(project._id, member._id);
+
+            await memberService.getMemberById(project._id, member._id)
+                .then((member) => assert(!member))
+                .catch(() => assert.fail());
+        });
+
+        it('should return code 200');
+
+        it('should remove code 404');
     });
 
-});
 
-after(function(done) {
-    mongoose.model('project').deleteMany({}, done);
 });
