a72ac041ee061774c9ad599b756783f0079027ee
Silverazz <alexandre.legendre@etu.u-bordeaux.fr>
2020-11-20 18:28:57 +0100

    Edit user-story + suppress user-story WIP

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -11,3 +11,22 @@ function closePopup() {
     wrapper.style.display = 'none';
     popups.forEach(p => p.style.display = 'none');
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function showDropdown() {
+    document.getElementById('myDropdown').classList.toggle('show');
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName('dropdown-content');
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
