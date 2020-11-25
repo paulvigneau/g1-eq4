ba4aa3eb816c160d60792570349e9bf7da3ce7cf
paulvigneau <paulvigneau98@gmail.com>
2020-11-25 22:16:12 +0100

    Add BadRequestError for missing params in routes

--- routes/backlog-routes.js
+++ routes/backlog-routes.js
@@ -4,39 +4,55 @@ const backlogService = require('../services/backlog.js');
 const sprintService = require('../services/sprint.js');
 const userStoryService = require('../services/user-story.js');
 const moment = require('moment');
+const { BadRequestError } = require('../errors/Error');
 
-router.get('/', function (req, res) {
-    backlogService.getAllSprints(req.params.id)
-        .then((backlog) => {
-            res.render('backlog', {
-                backlog: backlog,
-                projectId: req.params.id,
-                moment: moment
+router.get('/', function (req, res, next) {
+    if (req.params.id) {
+        backlogService.getAllSprints(req.params.id)
+            .then((backlog) => {
+                res.render('backlog', {
+                    backlog: backlog,
+                    projectId: req.params.id,
+                    moment: moment
+                });
+            })
+            .catch(() => {
+                res.redirect('/404');
             });
-        })
-        .catch(() => {
-            res.redirect('/404');
-    });
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 router.post('/sprint', function (req, res, next) {
-    sprintService.addSprint(req.params.id, req.body.start, req.body.end)
-    .then(() =>
-        res.status(200).send()
-    )
-    .catch((err) =>
-        next(err)
-    );
+    if (req.params.id && req.body.start && req.body.end) {
+        sprintService.addSprint(req.params.id, req.body.start, req.body.end)
+            .then(() =>
+                res.status(200).send()
+            )
+            .catch((err) =>
+                next(err)
+            );
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 router.delete('/sprints/:sprintId', function (req, res, next) {
-    sprintService.deleteSprint(req.params.id, req.params.sprintId)
-        .then(() =>
-            res.status(200).send()
-        )
-        .catch((err) =>
-            next(err)
-        );
+    if (req.params.id && req.params.sprintId) {
+        sprintService.deleteSprint(req.params.id, req.params.sprintId)
+            .then(() =>
+                res.status(200).send()
+            )
+            .catch((err) =>
+                next(err)
+            );
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 router.get('/new-sprint', function(req, res){
@@ -46,13 +62,18 @@ router.get('/new-sprint', function(req, res){
 }); // TODO Update e2e tests and delete this route
 
 router.post('/new-user-story', function (req, res, next) {
-    userStoryService.addUS(req.params.id, null, req.body.description, req.body.difficulty)
-        .then(() => {
-            res.status(200).send();
-        })
-        .catch((err) =>
-            next(err)
-        );
+    if (req.params.id && req.body.description && req.body.difficulty) {
+        userStoryService.addUS(req.params.id, null, req.body.description, req.body.difficulty)
+            .then(() => {
+                res.status(200).send();
+            })
+            .catch((err) =>
+                next(err)
+            );
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 router.get('/new-user-story', function(req, res){
@@ -62,26 +83,35 @@ router.get('/new-user-story', function(req, res){
 }); // TODO Update e2e tests and delete this route
 
 router.put('/user-story', function(req, res, next){
-    userStoryService.transferUS(req.params.id,
-        req.body.from, req.body.to, req.body.usId, req.body.index)
-        .then(() => {
-            res.status(200).send();
-        })
-        .catch((err) =>
-            next(err)
-        );
+    if (req.params.id && req.body.usId && req.body.index) {
+        userStoryService.transferUS(req.params.id, req.body.from, req.body.to, req.body.usId, req.body.index)
+            .then(() => {
+                res.status(200).send();
+            })
+            .catch((err) =>
+                next(err)
+            );
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 router.put('/:sprintId/:usId/close', function(req, res, next) {
-    userStoryService.closeUS(req.params.id, req.params.sprintId, req.params.usId)
-        .then(() => {
-            console.log('SprintID ' + req.params.sprintId);
-            console.log('usID ' + req.params.usId);
-            res.status(200).send();
-        })
-        .catch((err) =>
-            next(err)
-        );
+    if (req.params.id && req.params.sprintId && req.params.usId) {
+        userStoryService.closeUS(req.params.id, req.params.sprintId, req.params.usId)
+            .then(() => {
+                console.log('SprintID ' + req.params.sprintId);
+                console.log('usID ' + req.params.usId);
+                res.status(200).send();
+            })
+            .catch((err) =>
+                next(err)
+            );
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 module.exports = router;
--- routes/project-routes.js
+++ routes/project-routes.js
@@ -3,20 +3,26 @@ const moment = require('moment');
 const router = express.Router({ mergeParams: true });
 const projetService = require('../services/project');
 const memberService = require('../services/member');
+const { BadRequestError } = require('../errors/Error');
 
-router.get('/', function (req, res) {
-    projetService.getProjectByID(req.params.id)
-        .then((project) => {
-            res.render('project', {
-                project: project,
-                projectId: req.params.id,
-                moment: moment
-            });
-        })
-        .catch(() => {
-            res.redirect('/404');
-        }
-    );
+router.get('/', function (req, res, next) {
+    if (req.params.id) {
+        projetService.getProjectByID(req.params.id)
+            .then((project) => {
+                res.render('project', {
+                    project: project,
+                    projectId: req.params.id,
+                    moment: moment
+                });
+            })
+            .catch(() => {
+                    res.redirect('/404');
+                }
+            );
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 router.get('/new-member', function(req, res){
@@ -33,23 +39,33 @@ router.get('/new-member', function(req, res){
 }); // TODO Update e2e tests and delete this route
 
 router.post('/member', function (req, res, next) {
-    memberService.addMember(req.params.id, req.body.name, req.body.email, req.body.role)
-        .then(() => {
-            memberService.sendEmailToMember(req.params.id, req.body.name, req.body.email, req.body.role);
-            res.status(200).send();
-        })
-        .catch((err) => {
-            next(err);
-        });
+    if (req.params.id && req.body.name && req.body.email && req.body.role) {
+        memberService.addMember(req.params.id, req.body.name, req.body.email, req.body.role)
+            .then(() => {
+                memberService.sendEmailToMember(req.params.id, req.body.name, req.body.email, req.body.role);
+                res.status(200).send();
+            })
+            .catch((err) => {
+                next(err);
+            });
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 router.delete('/members/:memberId', function(req, res, next){
-    memberService.deleteMember(req.params.id, req.params.memberId)
-        .then(() =>
-            res.status(200).send()
-        ).catch((err) =>
+    if (req.params.id && req.params.memberId) {
+        memberService.deleteMember(req.params.id, req.params.memberId)
+            .then(() =>
+                res.status(200).send()
+            ).catch((err) =>
             next(err)
-    );
+        );
+    }
+    else {
+        next(new BadRequestError('Un ou plusieurs champs sont manquants.'));
+    }
 });
 
 const backlog = require('./backlog-routes');
