a2a48ab18f2d5c318ff5e64bb5d041ba1ab77642
Silverazz <alexandre.legendre@etu.u-bordeaux.fr>
2020-12-05 22:31:39 +0100

    [DEV] Continuing work on kanban panel, tasks.ejs and view-task.ejs - onchange still not working

--- public/javascript/tasks.js
+++ public/javascript/tasks.js
@@ -1,3 +1,52 @@
+const taskForm = document.querySelector('#edit-task-form');
+
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const data = new URLSearchParams(new FormData(taskForm));
+    // eslint-disable-next-line no-undef
+    sendForm('/tasks', data)
+        .then((resp) => {
+            if (resp.status === 400) {
+                resp.json().then(text => alert(text.message));
+            }
+            else
+                document.location.reload();
+        });
+});
+
+function cleanMembersOptions(){
+    let select = document.querySelector('select[id="edit-members"]');
+    let length = select.options.length - 1;
+    for(let i = length; i >= 0; i--) {
+        select.options.remove(i);
+    }
+}
+
+function createMemberOption(member){
+    let option = document.createElement('option');
+    option.text = member.name;
+    let select = document.querySelector('select[id="edit-members"]');
+    select.appendChild(option);
+}
+
+function renderMembers(projectMembers){
+    return function(event) {
+        console.log('test');
+        console.log(projectMembers)
+        cleanMembersOptions();
+        let type = getCurrentType();
+        let membersList = filterMembersByRole(projectMembers, type);
+        for (let i = 0; i < membersList.length; i++) {
+            createMemberOption(membersList[i]);
+        }
+    }
+}
+
+function getCurrentType(){
+    return document.querySelector('select[id="edit-type"]').value;
+}
+
 function checkMemberRoleTaskType(memberRole, taskType){
     if(memberRole === 'Développeur' && taskType === 'DEV'){
         return true;
@@ -8,10 +57,6 @@ function checkMemberRoleTaskType(memberRole, taskType){
     return false;
 }
 
-function filterMembersByRole(projectMembers, task){
-    return projectMembers.filter(member => checkMemberRoleTaskType(member.role, task.type));
-}
-
-function filterTasksByPhase(taskList, progressPhase){
-    return taskList.filter(task => task.status === progressPhase);
+function filterMembersByRole(projectMembers, type){
+    return projectMembers.filter(member => checkMemberRoleTaskType(member.role, type));
 }
--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const taskService = require('../services/taskService');
+const projectService = require('../services/projectService');
 const { BadRequestError } = require('../errors/Error');
 
 router.get('/', function (req, res) {
-    taskService.getAllTasks(req.params.id)
-        .then((tasks) => {
+    projectService.getProjectByID(req.params.id)
+        .then((project) => {
             res.render('tasks', {
                 projectId: req.params.id,
-                tasks: tasks
+                projectMembers: project.members,
+                tasksList: project.management.tasks
             });
         })
         .catch(() => {
-                res.redirect('/404');
-            }
-        );
+            res.redirect('/404');
+        });
 });
 
 router.post('/tasks', function (req, res, next) {
@@ -33,7 +34,7 @@ router.post('/tasks', function (req, res, next) {
     }
 });
 
-router.put('/:tasks', function (req, res, next) {
+router.put('/tasks', function (req, res, next) {
     if (req.body.name && req.body.description && req.body.start && req.body.end) {
         taskService.updateTask(req.params.id, req.body.description, req.body.type, req.body.cost,
             req.body.memberId, req.body.USList, req.body.dependencies)
