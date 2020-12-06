18e48a10f17bccbd73dab29820ad844bbf315c0d
paulvigneau <paulvigneau98@gmail.com>
2020-12-06 19:22:27 +0100

    [FIX] Apply linter fix

--- model/taskModel.js
+++ model/taskModel.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const UserStory = require('./userStoryModel');
-const Member = require('./memberModel');
 const ObjectId = mongoose.ObjectId;
 
 let TaskSchema = new Schema({
         description: { type: String, required: true },
-        type: { type: String, required: true, enum: ['GEN', 'DEV', 'TEST'] },
-        cost: { type: Number, required: true},
-        status: { type: String, required: true, enum: ['TODO', 'WIP', 'DONE']},
-        checklist: {type: [Boolean], required: true, default: [] },
-        member: {type: ObjectId },
+        type: { type: String, required: true, enum: [ 'GEN', 'DEV', 'TEST' ] },
+        cost: { type: Number, required: true },
+        status: { type: String, required: true, enum: [ 'TODO', 'WIP', 'DONE' ] },
+        checklist: { type: [ Boolean ], required: true, default: [] },
+        member: { type: ObjectId },
         USList: { type: [ ObjectId ] },
-        dependencies: {type: [ ObjectId ]  },
+        dependencies: { type: [ ObjectId ]  },
     }
 );
 
